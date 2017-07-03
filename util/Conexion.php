0069e04abc53d3144ee8a5ed1426fa60384593b4
klvst3r <klvst3r@gmail.com>
2017-07-03 17:39:49 -0500

    Establecer constantes en la conexión inicial

--- util/Conexion.php
+++ util/Conexion.php
@@ -7,15 +7,32 @@
  * Time: 14h30
  */
 
+define("BD_HOST","localhost");
+define("DB_PORT","3306");
+define("DB_NAME","store");
+define("DB_USERNAME","dev");
+define("DB_PASSWORD","desarrollo")
+
+/*
+try{
+	$dsn = "mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=utf8";
+	new PDO($dsn,DB_USERNAME,DB_PASSWORD);
+	}catch (PDOExeption $e){
+			die($e->getMessage());
+	}
+echo "Connection Successful"; 
+
+ */
+
 class Conexion{
 	public static function conectar(){
 		try{
-			$cn = new PDO("mysql:host=localhost;database=store", "root", "jaque");
+			$cn = new PDO("mysql:host=localhost;dbname=store", "root", "jaque");
 		}catch (PDOExeption $ex){
 			echo $ex->getMessage();
 		}
 		//return $cn;
-		echo "Conection Successful";
+		echo "Connection Successful"; 
 	}
 }
 
