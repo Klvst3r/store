b1c25e7d188409e3673208efe60865eb81289622
klvst3r <klvst3r@gmail.com>
2017-07-04 10:55:58 -0500

    Creació y configuracion del core como libreria

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 //Indicamos que es PHP toto el codigo (al abrir sin cerrar)
 
-echo "Right!";
+//echo "Right!";
+
+//Definir la ruta del proyecto
+define("RUTA_BASE",dirname(realpath(__FILE__))."/");
+//echo RUTA_BASE;
+
+include "libreria/core.php";
---
+++ libreria/core.php
@@ -0,0 +1,12 @@
+<?php
+//Motor del proyecto
+
+/**
+ *Created by Klvst3r
+ *User: Klvst3r
+ *Date: 2017-007-04
+ * Time: 10h50
+ * 
+ */
+
+echo "core";
