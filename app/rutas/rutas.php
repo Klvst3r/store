2449844bb777f5f102670e6b845dc61e466a42c9
klvst3r <klvst3r@gmail.com>
2017-07-04 11:39:51 -0500

    Invocacion de las rutas en cada submit o recarga en la aplicacion

--- app/rutas/rutas.php
+++ app/rutas/rutas.php
@@ -3,4 +3,18 @@
  * Archivo donde estaran las rutas disponibles en la aplicación
  */
 
-echo "rutas";
+//echo "rutas";
+/**
+ * Este archivo siempre se va a actualizar, cada vez que se añada un controlador agregaremos una ruta si es necesario
+ */
+//Nueva instancia de la clase Ruta
+$ruta = new Ruta();
+
+/**
+ * Establecemos las rutas de sus controladores respectivos
+ * Son los archivos que procesaran las rutas principales del proyecto
+ */
+$ruta->controladores(array(
+	"/"=>"PrincipalController",
+	"/usuarios"=>"UsuarioController"
+	));
--- libreria/Ruta.php
+++ libreria/Ruta.php
@@ -6,6 +6,25 @@
  */
 
 class Ruta {
-	
-	 
+	/**
+	 * Metodo que permite ingresar controladores con sus respectivas rutas
+	 * Enviara los controladores a la propiedad privada que indiquemos
+	 */
+	//Propiedad basica en un arreglo
+	private $_controladores = array();
+	public function controladores($controlador){
+		$this->_controladores[] = $controlador;
+		//Este archivo Siempre se estara llamando. al recargarsse la pagina
+		//llamada al metodo que hace el rpoceso de rutas
+		/**
+		 * La clase (Ruta) se llama cada vez que se recarga o abra una página en la aplicación
+		 *  self hace referencia a esta clase dinamica, que se enviara en cada recarga o enter a la URL
+		 */
+		self::submit();
+	}
+
+	public function submit(){
+		echo "Hola Submit";
+	}
+
 }
