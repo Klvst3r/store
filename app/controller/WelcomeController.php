2c4817e6f5ec81dc0bbb7d8824e2361ebce1985b
klvst3r <klvst3r@gmail.com>
2017-07-05 14:48:09 -0500

    Se concluye la inclusión de los modelos dinamicamente verificando si es directorio o archivo

--- app/controller/WelcomeController.php
+++ app/controller/WelcomeController.php
@@ -2,7 +2,7 @@
 
 class WelcomeController {
 	public function index(){
-		echo "Hola aqui estamos en el index";
+		//echo "Hola aqui estamos en el index";
 	}
 
 	
--- libreria/help/helps.php
+++ libreria/help/helps.php
@@ -1,12 +1,20 @@
 <?php
 
 /**
- * Incluimos los modelos
+ * Incluimos los modelos dinamicamente.
  */
 
 function includeModels(){
 	//Los modelos estan inlcuidos en el core por lo tanto pueden ser leidos desde cualquier archivo de la aplicación
-	$directorio = MODELS;
-
-	echo $directorio;
+	$directorio = opendir(MODELS);
+	//Leemos el directorio cuanstas veces exista el directorio
+	//echo $directorio;
+	//Cada vez que sea un archivo entra y en caso contrario no entre
+	while($archivo = readdir($directorio )){
+		//VErificamos si no es directorio, sera un archivo .php
+		if(!is_dir($archivo)){
+			//sin esto no puede funcionar
+			require_once MODELS.$archivo;
+		}//if
+	}//while
 }
