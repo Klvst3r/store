c1c79f9c468d50a00caadf8c80a93d88536a4fca
klvst3r <klvst3r@gmail.com>
2017-07-27 19:59:43 -0500

    Se agregan los include de admin head y menu del panel de administración

---
+++ view/admininclude/head.php
@@ -0,0 +1,15 @@
+	<title>Administración</title>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+	<link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+	<link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
