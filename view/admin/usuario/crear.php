7924b228cf22db74a2907f6da5467bd3f02c3b1c
klvst3r <klvst3r@gmail.com>
2017-07-31 10:26:31 -0500

    Se inicial formulario de usuarios

--- view/admin/usuario/crear.php
+++ view/admin/usuario/crear.php
@@ -17,8 +17,8 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">Nuevo Usuario | 
-                        <a href="<?php url('usuario'); ?>" class="btn btn-primary">
-                            <i class="fa fa-default"></i> Ver Listado</a>
+                    <a href="<?php url('usuario'); ?>" class="btn btn-default">
+                        <i class="fa fa-users"></i> Ver Listado</a>
                     </h1>
                 </div>
                 <!-- /.col-lg-12 -->
@@ -26,18 +26,29 @@
 
             <!-- Begin Work Area -->
             
+            <form action="" method="POST" role="form">
+              <legend>Form title</legend>
               
+              <div class="form-group">
+                  <label for="">label</label>
+                  <input type="text" class="form-control" id="" placeholder="Input field">
+              </div>
+              
+              
+              
+              <button type="submit" class="btn btn-primary">Submit</button>
+          </form>  
 
-            <!-- End Work Area -->
+          <!-- End Work Area -->
 
 
-        </div>
-        <!-- /#page-wrapper -->
+      </div>
+      <!-- /#page-wrapper -->
 
-    </div>
-    <!-- /#wrapper -->
+  </div>
+  <!-- /#wrapper -->
 
-    <?php  include(VISTA_RUTA."admininclude/scripts.php"); ?>
+  <?php  include(VISTA_RUTA."admininclude/scripts.php"); ?>
 
 </body>
 
